dd3e10a795840f4f55d6429f03ad82a066177bd8
diegogglez <diegomiller7@gmail.com>
2023-10-02 15:00:47 +0200

    add if_else.py

---
+++ conditionals/if_else.py
@@ -0,0 +1,27 @@
+
+#* Esctructura de un condicional:
+""" 
+if True:
+  # Se ejecuta la acción
+  
+if False:
+  # No se ejecuta la acción """
+
+edad = 17
+
+#! Python es un lenguaje identado, para salir del bloque de la condición tenemos que quitar un nivel al tabulado
+if edad >= 18:
+  print('Puedes pasar')
+  # Esto forma parte del if
+  # Esto forma parte del if
+  
+else:
+  print('No puedes pasar')
+  # Esto forma parte del else
+  # Esto forma parte del else
+
+# Este código ya no depende del condicional
+print('No forma parte de ninguna condición')
+
+    
+
