fbf450f22877c96811621dd031db3e1b63a27ed3
diegogglez <diegomiller7@gmail.com>
2023-10-02 18:18:27 +0200

    add logical_op.py

---
+++ operators/logical_op.py
@@ -0,0 +1,18 @@
+
+#* AND
+result1 = True & True #True
+result2 = False & True #False
+result3 = True & False #False
+result4 = False & False #False
+
+#* OR
+result5 = True | True #True
+result6 = False | True #True
+result7 = True | False #True
+result8 = False | False #False
+
+#* NOT
+result9 = not True #False
+result10 = not False #True
+
+print(result2)
