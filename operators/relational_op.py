df7bddaa08638a8c21e37b0f3f2e761621431007
diegogglez <diegomiller7@gmail.com>
2023-10-02 14:38:17 +0200

    add relational_op

---
+++ operators/relational_op.py
@@ -0,0 +1,16 @@
+
+#* Los operadores relacionales o de comparación devuelven valores boolean.
+#* Funciona exactamente igual que en JavaScript
+
+igual_que = 5 == 6 # False
+
+distinto_de = 5 != 6 # True
+
+mayor_que = 5 > 6 # False
+
+menor_que = 5 < 6 # True
+
+mayor_o_igual = 5 >= 6 # False
+
+menor_o_igual = 5 <= 6 # True
+
