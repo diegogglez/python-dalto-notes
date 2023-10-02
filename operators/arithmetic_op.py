f27bd393480d5215b106c132a81e901a3daa455c
diegogglez <diegomiller7@gmail.com>
2023-10-02 14:19:32 +0200

    add arithmetic_op

---
+++ operators/arithmetic_op.py
@@ -0,0 +1,22 @@
+
+#* suma y resta (+, -)
+suma = 12 + 5
+resta = 12 -5
+
+#* multiplicación y división (*, /)
+multi = 12 * 5
+division = 12 / 2 # Devuelve un dato flotante (float)
+
+#* potenciación (exponente) (**)
+exponente = 12**5
+
+#* división baja (//)
+division_baja = 12 // 5 # Devuelve un int redondeando la división hacia abajo
+
+#* resto o módulo (%)
+resto = 12 % 5
+
+# type() nos devuelve el tipo de dato
+tipo_de_dato = type(division_baja) 
+
+print(tipo_de_dato)
