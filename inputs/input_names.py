dea6026604a80f2fd386a5c89c963a55909da636
diegogglez <diegomiller7@gmail.com>
2023-10-03 22:05:30 +0200

    add inputs

---
+++ inputs/input_names.py
@@ -0,0 +1,5 @@
+# Pedirle un dato al usuario (por consola)
+name = input('che maestro, dame tu nombre: ') # input() siempre retorna un string
+
+# Mostrando el Dato
+print(f'El nombre es: {name}')
---
+++ inputs/input_numbers.py
@@ -0,0 +1,13 @@
+# Pedirle un número al usuario (por consola)
+number = input('Dame un número, capitán!: ')
+
+# Convirtiendo el número a int y lo multiplico por 2
+result_int = int(number) * 2
+
+# Convirtiendo el número a float y lo multiplico por 2
+result_float = float(number) * 2
+
+#? Es importante tener en cuenta el tipo de dato que estamos trabajando o nos dará errores
+
+# Mostrando el resultado
+print(result_float)
